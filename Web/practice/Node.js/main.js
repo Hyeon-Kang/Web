926c2afcfe17a83f26eda0344515235e3f2410fd
Hyeon <rkd099@naver.com>
2019-07-22 16:33:38 +0900

    Update main.js

--- Web/practice/Node.js/main.js
+++ Web/practice/Node.js/main.js
@@ -1,16 +1,18 @@
 var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
-  var url = request.url;
-  if (request.url == '/'){
-    url = '/index.html';
-  }
-  if(request.url == '/faiblcon.ico'){
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+
 });
 app.listen(3000);
-
 // 원본 : 생활코딩/module/3549/21032
