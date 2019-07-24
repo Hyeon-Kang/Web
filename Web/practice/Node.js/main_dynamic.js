df7913aea55019cbfaf853c4ad937172a6b28695
Hyeon <rkd099@naver.com>
2019-07-24 23:57:06 +0900

    Update main_dynamic.js

--- Web/practice/Node.js/main_dynamic.js
+++ Web/practice/Node.js/main_dynamic.js
@@ -13,6 +13,7 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
+    
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var template = `
       <!doctype html>
