cc0b71f8d483ae61b414f754256fe8a64c0cde8b
Hyeon <rkd099@naver.com>
2019-07-25 00:50:32 +0900

    Update main_dynamic.js
    404 error 페이지 추가

--- Web/practice/Node.js/main_dynamic.js
+++ Web/practice/Node.js/main_dynamic.js
@@ -5,37 +5,42 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+
+    console.log(rul.parse(_url, true)); // 경로가 유효하면 주소정보 출력
+
+    if(pathname === '/'){
+
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+
+    } else { // 오류페이지
+      response.writeHead(404);
+      response.end(template); // 원하는 페이지 등록
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+
 
 
 });
