c2889084af20602215cc63eab44fc7c91480cffe
Hyeon <rkd099@naver.com>
2019-07-31 22:34:34 +0900

    Create userThrows.java

---
+++ Web/practice/Java/userThrows.java
@@ -0,0 +1,21 @@
+public class Account {
+  private long balance;
+
+  public Account() {}
+
+  public long getBalance() {
+    return balance;
+  }
+
+  public void deposit(int money) {
+    balance += money;
+  }
+
+  public void withdraw(innt money) throws BalanceInsufficientExcepton {
+    if (balance < money) {
+      throw new BalanceInsufficientExcepton("잔고 부족 : " + (money - balance) + "모자람");
+    }
+    balance -= money;
+
+  }
+}
