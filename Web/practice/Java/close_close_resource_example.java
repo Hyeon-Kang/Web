716cc4b1097ece7c3500ba52094c3877c8961b7d
Hyeon <rkd099@naver.com>
2019-07-31 18:11:03 +0900

    Update close_close_resource_example.java

--- Web/practice/Java/close_close_resource_example.java
+++ Web/practice/Java/close_close_resource_example.java
@@ -16,3 +16,25 @@ public class close_resource_example implements AutoCloseable {
 
   } // main end
 } // class end
+
+
+// 위는 자바 6 이전 구형 코드
+
+
+ 자바 7 이후에 적용된 try-with-catch문을 이용하면 다음과 같이 편리해짐
+
+ try(FileInputStream fis = new FileInputStream ("file.txt") ) {
+   ...
+   fis.close(); // fis 입출력 객체 사용이 끝나면 close() 메소드로 닫아준다.
+ } catch (IOException e) {
+   ... 예외 대응 코드
+ }
+
+ 만약 입출력 객체를 여러개 선언해야 한 다면
+
+ try {
+   FileInputStream fis1 = new FileInputStream ("file1.txt");
+   FileInputStream fis2 = new FileInputStream ("file2.txt");
+ }
+
+ 요렇게 개별로 선언해주어도 가능
