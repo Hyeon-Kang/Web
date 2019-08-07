f795a893a63b3e21179da918b838a3237f06d4c3
Hyeon <rkd099@naver.com>
2019-08-07 23:44:38 +0900

    Update BeepPrintExample2.java

--- Web/practice/Java/멀티 스레드 예제/BeepPrintExample2.java
+++ Web/practice/Java/멀티 스레드 예제/BeepPrintExample2.java
@@ -4,5 +4,11 @@ public class BeepPrintExample2 {
   public static void main(String[] args) {
     Runnable beepTask = new BeepTask();
     Thread thread = new Thread(beepTask);
+
+    for (int i=0; i<5; i++) {
+      System.out.println("띵");
+      try {Thread.sleep(500);} catch (Exception e) {}
+
+    }
   }
 }
