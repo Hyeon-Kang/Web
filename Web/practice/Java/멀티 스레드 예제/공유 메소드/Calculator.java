b875de54c88247c0a635772a29907efcc9dd8314
Hyeon <rkd099@naver.com>
2019-08-13 00:09:01 +0900

    Update Calculator.java

--- Web/practice/Java/멀티 스레드 예제/공유 메소드/Calculator.java
+++ Web/practice/Java/멀티 스레드 예제/공유 메소드/Calculator.java
@@ -1,5 +1,6 @@
 package thread_synchronized_class;
 
+
 public class Calculator {
 	private int memory; // 공유 필드
 
@@ -12,8 +13,9 @@ public class Calculator {
 
 		try {
 			Thread.sleep(2000);
-		} catch (InterruptedException e) {
 			System.out.println(Thread.currentThread().getName() + ":" + this.memory);
+		} catch (InterruptedException e) {
+			//System.out.println(Thread.currentThread().getName() + ":" + this.memory);
 		}
 	} // setMemory end
 
