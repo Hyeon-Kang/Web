4695085e5a4f517c22770b0e0ab0dbd017a26f97
Hyeon <rkd099@naver.com>
2019-08-13 00:09:49 +0900

    Create MainThreadExample.java

---
+++ Web/practice/Java/멀티 스레드 예제/공유 메소드/MainThreadExample.java
@@ -0,0 +1,18 @@
+package thread_synchronized_class;
+
+public class MainThreadExample {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+
+		User1 user1 = new User1();
+		user1.setCalculator(calculator);
+		user1.start(); // thread running
+
+		User2 user2 = new User2();
+		user2.setCalculator(calculator);
+		user2.start();
+
+	}
+
+}
