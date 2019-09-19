444c2bafbb0f03b0e51985f87f3ae8b7b3767bbc
Hyeon <rkd099@naver.com>
2019-09-19 14:21:52 +0900

    remove package name

--- Web/practice/Java/데이터 통신 실습/week04/Recieve_thread.java
+++ Web/practice/Java/데이터 통신 실습/week04/Recieve_thread.java
@@ -1,4 +1,4 @@
-package week04;
+//ackage week04;
 
 // 수신 스레드
 import java.io.BufferedReader;
@@ -10,28 +10,28 @@ public class Recieve_thread extends Thread{
 	Socket sock;
 	public Recieve_thread(Socket sock) {
 		this.sock = sock;
-		
+
 	}
-	
+
 	public void run() {
 		try {
 			InputStream in = sock.getInputStream(); // 통신에 사용할 InputStream 객체 in 생성
 			// InputStream은 바이트 단위 인코딩, InputStreamReader는 문자단위 인코딩
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			// 입 출력 통신을 원활하게 하기 위한 버퍼 생성, 버퍼는 InputStreamReader로부터 문자 단위로 읽어온다.
-			String line; 
-			
+			String line;
+
 			while(true) {
 				if ((line= br.readLine()) != null) { // line에 버퍼 br내용 저장, 이것이 null 이 아니라면 문장 출력
 					System.out.println(sock.getInetAddress() + ":" + line);
 				}
 				if(line.contentEquals("quit")) break; // quit 입력시 탈출 (종료)
 			}
-			
+
 			br.close(); // 버퍼 자원 수납
-			
+
 			// 데이터를 받는 스트림 객체 in 은 close 안 하나??
-			
+
 		}catch (Exception e ) {
 			//
 		}
--- Web/practice/Java/데이터 통신 실습/week04/Send_thread.java
+++ Web/practice/Java/데이터 통신 실습/week04/Send_thread.java
@@ -1,4 +1,4 @@
-package week04;
+//package week04;
 
 
 // 송신 스레드 클래스
@@ -16,26 +16,26 @@ public class Send_thread extends Thread {
 	public Send_thread(Socket sock) { // 인자로 받은 소켓 정보와 송신 스레드 실행
 		this.sock = sock;
 	}
-	
+
 	public void run( ) {
 		try {
 			OutputStream out; // 송신 Stream 객체 out 생성
 			out = sock.getOutputStream(); // 전송 스트림 객체 out에 sock 데이터로 상대 정보 전달
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out)); 
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
 			// 입력을 받아 전송하는 객체, 전송 스트림 out과 연결됨
 			String line;
 			BufferedReader keyboard = new BufferedReader(new InputStreamReader (System.in));
 			// 키보드로부터 입력받은 데이터를 버퍼 객체에 저장
-			
+
 			while(true) {
 				if ((line=keyboard.readLine()) != null) { // 키보드 버퍼에 데이터가 있다면(들어왔다면)
 					pw.println(line); // pw에 전달 후
 					pw.flush(); // pw에 들어있는 모든 데이터를 출력 후 초기화(비우기)
 				}
-				
+
 				if(line.equals("quit")) break; // quit 입력시 종료
 			}
-			
+
 			pw.close(); // pw 자원 회수
 		} catch (IOException e) {
 			e.printStackTrace();
