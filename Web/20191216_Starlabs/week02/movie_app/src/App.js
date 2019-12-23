85224338bd4fba46730550f51a94f0e7549e0a75
Hyeon <rkd099@naver.com>
2019-12-23 14:33:42 +0900

    페이지로드 일정 시간 후 작업 수행
    settime

--- Web/20191216_Starlabs/week02/movie_app/src/App.js
+++ Web/20191216_Starlabs/week02/movie_app/src/App.js
@@ -4,25 +4,7 @@ import Movie from './Movie';
 
 
 
-const movies = [
-    {
-        title : "Matrix",
-        poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F66693b11a2874f5aa3f4d250b1830b261568780107021"
-    },
-    {
-        title : "Full Metal Jacket",
-        poster: "https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_700,q_80,w_490/v1485423697/bvacgjpzkogqdbcbo506.jpg"
-    },
-    {
-        title : "Old boy",
-        poster : "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-    },
-    {
-        title : "Star wars",
-        poster: "https://i.ytimg.com/vi/ZXva4Z0zVK8/maxresdefault.jpg"
-    }
 
-]
 
 // 몇 개인지 모르는 리스트 출력
 
@@ -32,8 +14,29 @@ class App extends Component {
     // Render : componentWillMount() -> render() -> componentDidMount()
     // Update componentWillReciveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> component()
     state = {
-        greeting: 'helloo'
+        greeting: 'helloo',
+        movies: [
+            {
+                title : "Matrix",
+                poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F66693b11a2874f5aa3f4d250b1830b261568780107021"
+            },
+            {
+                title : "Full Metal Jacket",
+                poster: "https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_700,q_80,w_490/v1485423697/bvacgjpzkogqdbcbo506.jpg"
+            },
+            {
+                title : "Old boy",
+                poster : "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+            },
+            {
+                title : "Star wars",
+                poster: "https://i.ytimg.com/vi/ZXva4Z0zVK8/maxresdefault.jpg"
+            }
+
+        ]
     }
+
+    // 컴포넌트 내부 자원은 state 속성이 붙는다.
 
     componentDidMount(){
         setTimeout(() => {
@@ -42,12 +45,16 @@ class App extends Component {
                 greeting: 'Hello again! '// 5000ms 후에 helloo -> hello again!으로 갱신
             })
         }, 5000)
+
+        setTimeout(function() {
+            console.log('hello my console!')
+        }, 3000)
     }
     render() {
         return (
             <div className="App">
                 {this.state.greeting}
-                {movies.map((movie, index) => {
+                {this.state.movies.map((movie, index) => {
                     return <Movie title={movie.title} poster={movie.poster} key={index} />
                 })}
             </div>
