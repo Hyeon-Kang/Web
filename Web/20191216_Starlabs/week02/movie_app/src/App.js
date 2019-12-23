3dbdf5e6eab633749ec67a9ebf82dcbd1e0c9773
Hyeon <rkd099@naver.com>
2019-12-23 13:23:30 +0900

    텍스트와 이미지 나열

--- Web/20191216_Starlabs/week02/movie_app/src/App.js
+++ Web/20191216_Starlabs/week02/movie_app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 연속 데이터 전달
+const movieTitles = [
+    "Metrix",
+    "Full Metal Jacket",
+    "Old boy",
+    "Star wars"
+]
+
+const movieImages = [
+    "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
+    "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fstar-wars-jedi-fallen-order%2Fhome%2FSWFJO_Standard_Keyart_1920x1080-1920x1080-7a42241ea451299ac73036ac61eb73c1184753e0.jpg"
+]
+
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <Movie title={movieTitles[0]} poster={movieImages[0]} />
+            <Movie title={movieTitles[1]} poster={movieImages[1]} />
+            <Movie title={movieTitles[2]} poster={movieImages[2]} />
+            <Movie title={movieTitles[3]} poster={movieImages[3]} />
+            </div>
+        )
+    }
 }
 
 export default App;
